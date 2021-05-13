17c64f908a5eabefd12a7c59eae8b72a9518b12f
Nicholas McClain-Simpson <n.mcsimp@gmail.com>
2021-05-13 14:43:55 -0500

    Followed teams list

--- src/app/teams/teams.page.ts
+++ src/app/teams/teams.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
-// import { Team, TeamsService } from '../services/teams.service';
+import { LoadingController, NavController } from '@ionic/angular';
 import { Team, Tournament, TournamentData, TournamentsService } from '../services/tournaments.service';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-folder',
@@ -11,42 +12,66 @@ import { Team, Tournament, TournamentData, TournamentsService } from '../service
 })
 export class TeamsPage implements OnInit {
   public title: string;
+  public isTournament: boolean = false;
   // List of Unique List pages
   public teams_identifier_list = [
     'My Teams',
     'All Teams',
+  ]  
+  public favorites = [
+    {
+      team: { id: 6182, name: 'HC Elite 7th', coach: "Pimpson" },
+      tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+      tournamentName: 'March Madness Tournament'
+    },
+    {
+      team: { id: 805, name: 'HC Elite', coach: "Pimpson" },
+      tournamentId: '98c6857e-b0d1-4295-b89e-2d95a45437f2',
+      tournamentName: 'Holiday Hoops Challenge'
+    }
   ]
-  // Map of teams to display for tournaments
-  // public teams_tournaments = {
-  //   'March Madness': [1,2],
-  //   'Stanley Cup': [3,4],
-  // };
-  public tournament: Tournament;
-  public teams: Array<Team>;
-  public data: TournamentData;
 
-  constructor(private activatedRoute: ActivatedRoute, private nav: NavController, 
+  // For Tournaments
+  public tournament: Tournament;
+  public teams: Team[];
+  public data: TournamentData;
+  public allTeamDivisions: any[];
+
+  constructor(private activatedRoute: ActivatedRoute, private loadingController: LoadingController, 
     public tournamentsService: TournamentsService) { }
 
   ngOnInit() {
     const identifier = this.activatedRoute.snapshot.paramMap.get('id');
 
-    // debugger;
     if (this.teams_identifier_list.includes(identifier)) {
       this.title = identifier;
-      this.teams = [];
-    } else {
-      // This is a tournament ID, so search for the data
-      this.tournamentsService.getTournamentData(identifier).subscribe((data: TournamentData) => {
-        this.data = data;
-        this.teams = data.teams;
-        this.teams.sort((a, b) => (a.name > b.name) ? 1 : -1);
-        this.tournament = data.tournament;
-        this.title = this.tournament.name;
-      });
-
-      // this.teams = this.teamService.getByIds(this.teams_tournaments[identifier]);
+      // Set Favorites
+    } else { // Is Tournament ID:
+      this.loadTournamentData(identifier);
     }
   }
 
+  async loadTournamentData(identifier) {
+    const loading = await this.loadingController.create({
+      message: "Getting Team Details..."
+    });
+
+    await loading.present();
+    
+    this.tournamentsService.getTournamentData(identifier).subscribe((data: TournamentData) => {
+      this.isTournament = true;
+      this.data = data;
+      this.teams = data.teams;
+      this.teams.sort((a, b) => (a.name > b.name) ? 1 : -1);
+
+      this.allTeamDivisions = _.chain(this.teams).groupBy('division').toPairs().map(
+        item => _.zipObject(['divisionName', 'divisionTeams'], item)
+      ).value();
+
+      this.tournament = data.tournament;
+      this.title = this.tournament.name;
+
+      loading.dismiss();
+    });
+  }
 }
